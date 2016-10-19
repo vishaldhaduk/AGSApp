9b25d0c94842bc640f506d2cbcc5de066905f15e
vishaldhaduk <vishal.dhaduk11@gmail.com>
2016-10-19 08:10:58 -0400

    Api calling feature added

--- app/src/main/java/com/example/vishaldhaduk/ags/MainActivity.java
+++ app/src/main/java/com/example/vishaldhaduk/ags/MainActivity.java
@@ -8,12 +8,37 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
-public class MainActivity extends AppCompatActivity implements OnClickListener {
-    private Button scanBtn;
+//public class MainActivity extends AppCompatActivity implements OnClickListener {
+    public class MainActivity extends AppCompatActivity  implements OnClickListener {
+
+    static final String API_KEY = "USE_YOUR_OWN_API_KEY";
+    static final String API_URL = "https://jsonplaceholder.typicode.com";
+    EditText emailText;
+    TextView responseView;
+    ProgressBar progressBar;
+    String email;
+        private Button scanBtn;
     private TextView formatTxt, contentTxt;
 
     @Override
@@ -24,13 +49,28 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         formatTxt = (TextView) findViewById(R.id.scan_format);
         contentTxt = (TextView) findViewById(R.id.scan_content);
         scanBtn.setOnClickListener(this);
+
+        responseView = (TextView) findViewById(R.id.responseView);
+        emailText = (EditText) findViewById(R.id.emailText);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        Button queryButton = (Button) findViewById(R.id.queryButton);
+
+        queryButton.setOnClickListener(this);
+       /* queryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new RetrieveFeedTask().execute();
+            }
+        }); */
     }
 
-    public void onClick(View v) {
+     public void onClick(View v) {
         if (v.getId() == R.id.scan_button) {
             IntentIntegrator scanIntegrator = new IntentIntegrator(this);
             scanIntegrator.initiateScan();
         }
+         else { new RetrieveFeedTask().execute();}
     }
 
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
@@ -44,6 +84,68 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
             Toast toast = Toast.makeText(getApplicationContext(),
                     "No scan data received!", Toast.LENGTH_SHORT);
             toast.show();
+        }
+    }
+
+    public class RetrieveFeedTask extends AsyncTask<Void, Void, String> {
+
+        private Exception exception;
+
+        protected void onPreExecute() {
+            progressBar.setVisibility(View.VISIBLE);
+            responseView.setText("");
+            email = emailText.getText().toString();
+        }
+
+        protected String doInBackground(Void... urls) {
+
+            // Do some validation here
+            try {
+                URL url = new URL(API_URL);
+                //URL url = new URL(API_URL + "email=" + email + "&apiKey=" + API_KEY);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line).append("\n");
+                    }
+                    bufferedReader.close();
+                    return stringBuilder.toString();
+                }
+                finally{
+                    urlConnection.disconnect();
+                }
+            }
+            catch(Exception e) {
+                Log.e("ERROR", e.getMessage(), e);
+                return null;
+            }
+        }
+
+        protected void onPostExecute(String response) {
+            if(response == null) {
+                response = "THERE WAS AN ERROR";
+            }
+            progressBar.setVisibility(View.GONE);
+            Log.i("INFO", response);
+            responseView.setText(response);
+            // TODO: check this.exception
+            // TODO: do something with the feed
+
+//            try {
+//                JSONObject object = (JSONObject) new JSONTokener(response).nextValue();
+//                String requestID = object.getString("requestId");
+//                int likelihood = object.getInt("likelihood");
+//                JSONArray photos = object.getJSONArray("photos");
+//                .
+//                .
+//                .
+//                .
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
         }
     }
 }
